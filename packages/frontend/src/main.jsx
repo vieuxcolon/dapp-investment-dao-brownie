ddd91f9bec5dff1c497ed618d15c35884593262e
vieuxcolon <vieuxcolon@hotmail.fr>
2026-01-18 13:34:33 +0100

    Update main.jsx

--- packages/frontend/src/main.jsx
+++ packages/frontend/src/main.jsx
@@ -1,12 +1,9 @@
-import { ethers } from "ethers";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-const provider = new ethers.BrowserProvider(window.ethereum);
-
-async function connectWallet() {
-    await provider.send("eth_requestAccounts", []);
-    const signer = await provider.getSigner();
-    console.log("Connected account:", await signer.getAddress());
-}
-
-document.body.innerHTML = `<button id="connect">Connect Wallet</button>`;
-document.getElementById("connect").onclick = connectWallet;
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
